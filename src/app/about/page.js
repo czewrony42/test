1bb150f4ebb137a3df3f1a7dd2ed42d4e81fd422
czewrony42 <czewrony42@gmail.com>
2024-11-07 10:24:46 +0100

    strona about

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return(
+    <>
+    <h1 className="bg-red-300 text-center">O mnie</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id harum doloremque provident officia! Veritatis beatae vel quod optio porro corporis sapiente consequatur asperiores, ullam exercitationem illo perspiciatis deserunt expedita nam?
+    Consequuntur, laborum. Tempore aliquam tenetur aut. Enim sint, laborum exercitationem deserunt possimus fugiat tempora at debitis eveniet voluptate dolorum natus aliquam nam adipisci inventore velit! Rerum accusamus aliquid dicta quidem!
+    Blanditiis, facere reprehenderit. Quidem, aliquam! At voluptates iure laudantium magnam architecto, fugiat doloribus dolore accusantium maxime modi, inventore nobis perferendis nesciunt eius quia in necessitatibus quaerat qui commodi aliquid! Voluptatum.
+    Recusandae veniam, dolore explicabo accusantium maiores, error sit saepe mollitia dolores reiciendis ea. Mollitia illo voluptas recusandae ipsam ullam fuga minus, soluta nesciunt delectus, excepturi obcaecati et eligendi ex aspernatur.
+    Quos delectus eius tenetur? Eligendi unde, tempora esse, perferendis, dolor nemo debitis quas sed soluta architecto provident consectetur corrupti qui repudiandae error voluptas eveniet ab impedit ad cum pariatur consequatur!</p>
+    </>
+  )
+}
